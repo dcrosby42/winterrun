7d2dc1e4e3c32264aa27a11eb40cb8f5f9a06d25
David Crosby <dcrosby42@gmail.com>
2021-04-10 17:24:14 -0400

    Docs

--- lib/cedar/output.rb
+++ lib/cedar/output.rb
@@ -1,4 +1,10 @@
 module Cedar
+  # Output is a structural abstraction around the graphical output context.
+  # #window provides access to the Gosu::Window object for introspection purpises.
+  # #graphics is the root Cedar::Draw::Group; drawing is accomplished by appending
+  # parameterized drawing instructions to #graphics, eg:
+  #     output.graphics << Draw::Rect.new(...)
+  #     output.graphics << Draw::Label.new(...)
   class Output
     attr_reader :graphics, :window
 
@@ -7,6 +13,9 @@ module Cedar
       @graphics = Cedar::Draw::Group.new
     end
 
+    # Clears the root draw group.
+    # Called by the framework just before each frame is drawn; user code
+    # doesn't need to use this.
     def reset
       @graphics.clear
     end
